e310884f4f2521e02b8efddf71aefae2aa236c40
Kirill Morozov <kirill@morozov.group>
2020-02-03 01:52:20 -0800

    Implement minimal for use.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <phpcpp.h>
 
 
-template <class Container>
-void split_string(const std::string& str, Container& cont,
-            char delim = ' ')
-{
+template<class Container>
+void split_string(const std::string &str, Container &cont,
+                  char delim = ' ') {
     std::size_t current, previous = 0;
     current = str.find(delim);
     while (current != std::string::npos) {
@@ -15,21 +14,20 @@ void split_string(const std::string& str, Container& cont,
     cont.push_back(str.substr(previous, current - previous));
 }
 
-class DataObject : public Php::Base, public Php::ArrayAccess
-{
+class DataObject : public Php::Base, public Php::ArrayAccess {
 protected:
-    std::map<std::string, Php::Value> _data;
-    static std::map<std::string, std::string> _underscoreCache;
+    std::map <std::string, Php::Value> _data;
+    static std::map <std::string, std::string> _underscoreCache;
 public:
     DataObject() {};
+
     virtual ~DataObject() {};
 
     /**
      *  php "constructor"
      *  @param  params
      */
-    void __construct(Php::Parameters &params)
-    {
+    void __construct(Php::Parameters &params) {
         if (!params.empty()) {
             _data = params[0];
         } //_value = params[0];
@@ -37,8 +35,7 @@ public:
 
     Php::Value addData(Php::Parameters &params) {
 
-        for (auto &iter : params[0])
-        {
+        for (auto &iter : params[0]) {
             _data[iter.first] = iter.second;
             // output key and value
             // Php::out << iter.first << ": " << iter.second << std::endl;
@@ -48,10 +45,10 @@ public:
     };
 
     Php::Value setData(Php::Parameters &params) {
-        if(params.size() == 1 && params[0].isArray()) {
+        if (params.size() == 1 && params[0].isArray()) {
             _data = params[0];
         }
-        if(params.size() == 2 && params[0].isString()) {
+        if (params.size() == 2 && params[0].isString()) {
             std::string name = params[0];
             Php::Value value = params[1];
             _data[name] = value;
@@ -62,14 +59,14 @@ public:
     };
 
     Php::Value unsetData(Php::Parameters &params) {
-        if(params.empty()) {
+        if (params.empty()) {
             _data.clear();
         } else if (params[0].isString()) {
             std::string key = params[0];
             _data.erase(key);
         } else if (params[0].isArray()) {
-            std::vector<std::string> keys = params[0];
-            for(auto key : keys) {
+            std::vector <std::string> keys = params[0];
+            for (auto key : keys) {
                 _data.erase(key);
             }
         }
@@ -77,7 +74,7 @@ public:
     }
 
     Php::Value getData(Php::Parameters &params) {
-        if(params.empty()) {
+        if (params.empty()) {
             return _data;
         }
         std::string key = params[0];
@@ -87,9 +84,9 @@ public:
             return this->_getData(key);
         }
 
-            if (params[0].isArray()) {
-            std::vector<std::string> keys = params[0];
-            for(auto key : keys) {
+        if (params[0].isArray()) {
+            std::vector <std::string> keys = params[0];
+            for (auto key : keys) {
                 _data.erase(key);
             }
         }
@@ -102,13 +99,13 @@ public:
     }
 
     Php::Value getDataByPath(std::string &path) {
-        std::vector<std::string> keys;
+        std::vector <std::string> keys;
         split_string(path, keys);
 
-        std::map<std::string, Php::Value> res_data = _data;
+        std::map <std::string, Php::Value> res_data = _data;
         Php::Value data = _data;
-        for(auto key : keys) {
-            if(data.isArray() && (res_data.find(key) != res_data.end()) ) {
+        for (auto key : keys) {
+            if (data.isArray() && (res_data.find(key) != res_data.end())) {
                 data = res_data[key];
                 if (data.isArray()) {
                     res_data = data;
@@ -142,10 +139,43 @@ public:
         return nullptr;
     }
 
+    Php::Value setDataUsingMethod(Php::Parameters &params) {
+        return this;
+    }
+
+    Php::Value getDataUsingMethod(Php::Parameters &params) {
+        return nullptr;
+    }
+
+    Php::Value hasData(Php::Parameters &params) {
+        return nullptr;
+    }
+
+    Php::Value toArray(Php::Parameters &params) {
+        return nullptr;
+    }
+
+    Php::Value toXml(Php::Parameters &params) {
+        return nullptr;
+    }
+
+    Php::Value toJson(Php::Parameters &params) {
+        return nullptr;
+    }
+
+    Php::Value toString(Php::Parameters &params) {
+        return nullptr;
+    }
+
+    Php::Value __call(const char *name, Php::Parameters &params)
+    {
+        return nullptr;
+    }
+
 
     Php::Value _vall() {
         //int s = _data.size();
-        return (int)_data.size();
+        return (int) _data.size();
     };
 
     /**
@@ -154,8 +184,7 @@ public:
      *  @param  key
      *  @return bool
      */
-    virtual bool offsetExists(const Php::Value &key) override
-    {
+    virtual bool offsetExists(const Php::Value &key) override {
         return _data.find(key) != _data.end();
     }
 
@@ -164,8 +193,7 @@ public:
      *  @param  key
      *  @param  value
      */
-    virtual void offsetSet(const Php::Value &key, const Php::Value &value) override
-    {
+    virtual void offsetSet(const Php::Value &key, const Php::Value &value) override {
         _data[key] = value.stringValue();
     }
 
@@ -174,8 +202,7 @@ public:
      *  @param  key
      *  @return value
      */
-    virtual Php::Value offsetGet(const Php::Value &key) override
-    {
+    virtual Php::Value offsetGet(const Php::Value &key) override {
         if (_data.find(key) != _data.end()) {
             return _data[key];
         }
@@ -186,8 +213,7 @@ public:
      *  Remove a member
      *  @param key
      */
-    virtual void offsetUnset(const Php::Value &key) override
-    {
+    virtual void offsetUnset(const Php::Value &key) override {
         _data.erase(key);
     }
 };
@@ -195,8 +221,7 @@ public:
 /**
  *  Counter class that can be used for counting
  */
-class Counter : public Php::Base
-{
+class Counter : public Php::Base {
 private:
     /**
      *  The internal value
@@ -209,6 +234,7 @@ public:
      *  C++ constructor and destructor
      */
     Counter() {}
+
     virtual ~Counter() {}
 
     /**
@@ -217,9 +243,8 @@ public:
      *  @param  int     Optional increment value
      *  @return int     New value
      */
-    Php::Value increment(Php::Parameters &params)
-    {
-        return _value += params.empty() ? 1 : (int)params[0];
+    Php::Value increment(Php::Parameters &params) {
+        return _value += params.empty() ? 1 : (int) params[0];
     }
 
     /**
@@ -228,17 +253,15 @@ public:
      *  @param  int     Optional decrement value
      *  @return int     New value
      */
-    Php::Value decrement(Php::Parameters &params)
-    {
-        return _value -= params.empty() ? 1 : (int)params[0];
+    Php::Value decrement(Php::Parameters &params) {
+        return _value -= params.empty() ? 1 : (int) params[0];
     }
 
     /**
      *  Method to retrieve the current value
      *  @return int
      */
-    Php::Value value() const
-    {
+    Php::Value value() const {
         return _value;
     }
 };
@@ -247,80 +270,111 @@ public:
  *  tell the compiler that the get_module is a pure C function
  */
 extern "C" {
-    
-    /**
-     *  Function that is called by PHP right after the PHP process
-     *  has started, and that returns an address of an internal PHP
-     *  strucure with all the details and features of your extension
-     *
-     *  @return void*   a pointer to an address that is understood by PHP
-     */
-    PHPCPP_EXPORT void *get_module() 
-    {
-        // static(!) Php::Extension object that should stay in memory
-        // for the entire duration of the process (that's why it's static)
-        static Php::Extension extension("morozov_m2_booster", "1.0");
-        
-        // @todo    add your own functions, classes, namespaces to the extension
-        // description of the class so that PHP knows which methods are accessible
-        Php::Class<Counter> counter("Some\\Counter");
 
-        // register the increment method, and specify its parameters
-        counter.method<&Counter::increment>("increment", {
-                Php::ByVal("change", Php::Type::Numeric, false)
-        });
+/**
+ *  Function that is called by PHP right after the PHP process
+ *  has started, and that returns an address of an internal PHP
+ *  strucure with all the details and features of your extension
+ *
+ *  @return void*   a pointer to an address that is understood by PHP
+ */
+PHPCPP_EXPORT void *get_module() {
+    // static(!) Php::Extension object that should stay in memory
+    // for the entire duration of the process (that's why it's static)
+    static Php::Extension extension("morozov_m2_booster", "1.0");
 
-        // register the decrement, and specify its parameters
-        counter.method<&Counter::decrement>("decrement", {
-                Php::ByVal("change", Php::Type::Numeric, false)
-        });
+    // @todo    add your own functions, classes, namespaces to the extension
+    // description of the class so that PHP knows which methods are accessible
+    Php::Class <Counter> counter("Some\\Counter");
 
-        // register the value method
-        counter.method<&Counter::value>("value", {});
+    // register the increment method, and specify its parameters
+    counter.method<&Counter::increment>("increment", {
+            Php::ByVal("change", Php::Type::Numeric, false)
+    });
 
-        //Php::Namespace myNamespace("myNamespace");
+    // register the decrement, and specify its parameters
+    counter.method<&Counter::decrement>("decrement", {
+            Php::ByVal("change", Php::Type::Numeric, false)
+    });
 
-        Php::Class<DataObject> data_obj("Magento\\Framework\\DataObject");
-        data_obj.method<&DataObject::__construct>("__construct",{
+    // register the value method
+    counter.method<&Counter::value>("value", {});
+
+    //Php::Namespace myNamespace("myNamespace");
+
+    Php::Class <DataObject> data_obj("Magento\\Framework\\DataObject");
+    data_obj.method<&DataObject::__construct>("__construct", {
             Php::ByVal("arr", Php::Type::Array, false)
-        });
-        // register the decrement, and specify its parameters
+    });
+    // register the decrement, and specify its parameters
 //        data_obj.method<&DataObject::addData>("addData");
 
-        data_obj.method<&DataObject::addData>("addData", {
-                Php::ByVal("arr", Php::Type::Array, true)
-        });
+    data_obj.method<&DataObject::addData>("addData", {
+            Php::ByVal("arr", Php::Type::Array, true)
+    });
 
-        data_obj.method<&DataObject::setData>("setData", {
-                Php::ByVal("name", Php::Type::Null, true),
-                Php::ByVal("value", Php::Type::Null, false)
-        });
+    data_obj.method<&DataObject::setData>("setData", {
+            Php::ByVal("name", Php::Type::Null, true),
+            Php::ByVal("value", Php::Type::Null, false)
+    });
 
-        data_obj.method<&DataObject::unsetData>("unsetData", {
-                Php::ByVal("key", Php::Type::Null, false)
-        });
+    data_obj.method<&DataObject::unsetData>("unsetData", {
+            Php::ByVal("key", Php::Type::Null, false)
+    });
 
-        data_obj.method<&DataObject::getData>("getData", {
-                Php::ByVal("key", Php::Type::String, false)
-        });
+    data_obj.method<&DataObject::getData>("getData", {
+            Php::ByVal("key", Php::Type::String, false)
+    });
 
-        data_obj.method<&DataObject::getDataByPath>("getDataByPath", {
-                Php::ByVal("key", Php::Type::String, true)
-        });
+    data_obj.method<&DataObject::getDataByPath>("getDataByPath", {
+            Php::ByVal("key", Php::Type::String, true)
+    });
 
-        data_obj.method<&DataObject::getDataByKey>("getDataByKey", {
-                Php::ByVal("key", Php::Type::String, true)
-        });
+    data_obj.method<&DataObject::getDataByKey>("getDataByKey", {
+            Php::ByVal("key", Php::Type::String, true)
+    });
 
-        data_obj.method<&DataObject::_vall>("_vall", {
-        });
+    data_obj.method<&DataObject::setDataUsingMethod>("setDataUsingMethod", {
+            Php::ByVal("key", Php::Type::String, true),
+            Php::ByVal("args", Php::Type::Array, false)
+
+    });
+
+    data_obj.method<&DataObject::getDataUsingMethod>("getDataUsingMethod", {
+            Php::ByVal("key", Php::Type::String, true),
+            Php::ByVal("args", Php::Type::Null, false)
+    });
+
+    data_obj.method<&DataObject::hasData>("hasData", {
+            Php::ByVal("key", Php::Type::String, false)
+    });
+
+    data_obj.method<&DataObject::toArray>("toArray", {
+            Php::ByVal("keys", Php::Type::Array, false)
+    });
+
+    data_obj.method<&DataObject::toXml>("toXml", {
+            Php::ByVal("keys", Php::Type::Array, false)
+    });
+
+    data_obj.method<&DataObject::toJson>("toJson", {
+            Php::ByVal("keys", Php::Type::Array, false)
+    });
+
+    data_obj.method<&DataObject::toString>("toString", {
+            Php::ByVal("format", Php::Type::String, false)
+    });
 
 
-        // add the class to the extension
-        extension.add(std::move(counter));
-        extension.add(std::move(data_obj));
+    data_obj.method<&DataObject::_vall>("_vall", {
+    });
 
-        // return the extension
-        return extension;
-    }
+
+    // add the class to the extension
+    extension.add(std::move(counter));
+    extension.add(std::move(data_obj));
+
+    // return the extension
+    return extension;
+}
 }
